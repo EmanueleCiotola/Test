371b2d894308644e282c0bcf610538aafa6fca56
Devma <66367222+EmanueleCiotola@users.noreply.github.com>
2024-02-13 00:49:34 +0100

    Modifica animazione loading page e navbar
    Probabilmente da rivedere

--- javascript/main.js
+++ javascript/main.js
@@ -1,34 +1,41 @@
 // gestione degli items osservati dopo il caricamento della pagina (utile per animazioni)
 const loadingScreen = document.getElementById("loadingScreen");
+const loadingScreenLogo = document.getElementById("loadingScreen__logo");
 window.addEventListener("load", function() {
-
     // loadingScreen di almeno un secondo anche con caricamenti veloci
     var loadTime = Date.now() - performance.timeOrigin;
     var delay = Math.max(2000 - loadTime, 0);
 
     setTimeout(function() {
-        // rimozione loadingScreen
-        loadingScreen.classList.add("loadingScreen--hidden");
-
-        // gestione items osservati
-        var elementiDaOsservare = document.querySelectorAll('.osservato');
-        var callback = function(items) {
-            items.forEach((item) => {
-                var position = item.target.getBoundingClientRect();
-                if (item.isIntersecting) {
-                    item.target.classList.add("mostrato");
-                } else if (position.top > 0) { // rimuove la classe mostrato solo se l'item si trova nella parte non visibile superiore dello schermo
-                    item.target.classList.remove("mostrato");
-                }
+        //inizia animazione opacità
+        loadingScreenLogo.classList.remove("loadingScreen__logo--inMovimento");
+        loadingScreen.classList.add("loadingScreen--loaded");
+        
+        // animazione opacità prima di rimuovere
+        setTimeout(() => {
+            // rimozione loadingScreen
+            loadingScreen.classList.add("loadingScreen--hidden");
+        
+            // gestione items osservati
+            var elementiDaOsservare = document.querySelectorAll('.osservato');
+            var callback = function(items) {
+                items.forEach((item) => {
+                    var position = item.target.getBoundingClientRect();
+                    if (item.isIntersecting) {
+                        item.target.classList.add("mostrato");
+                    } else if (position.top > 0) { // rimuove la classe mostrato solo se l'item si trova nella parte non visibile superiore dello schermo
+                        item.target.classList.remove("mostrato");
+                    }
+                });
+            }
+            var observer = new IntersectionObserver(callback, {threshold: 0});
+            elementiDaOsservare.forEach((element) => {
+                observer.observe(element);
             });
-        }
-        var observer = new IntersectionObserver(callback, {threshold: 0});
-        elementiDaOsservare.forEach((element) => {
-            observer.observe(element);
-        });
 
-        // permetti overflow nella pagina caricata
-        document.body.classList.remove("body--loading");
+            // permetti overflow nella pagina caricata
+            document.body.classList.remove("body--loading");
+        }, 250);
     }, delay);
 });
 
